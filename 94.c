b66d285618dd221c45b5e95f8149606f5622396e
zhengdongqi <1821260963@qq.com>
2019-05-23 23:59:50 +0800

    计算BMI

---
+++ 94.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: 94.c
+	> Author: zhengdongqi
+	> Mail: 1821260963@qq.com
+	> Created Time: 四  5/23 23:24:49 2019
+ ************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a, b;
+    scanf("%lf %lf", &a, &b);
+    printf("%.2lf\n", a / (b * b));
+    return 0;
+}
