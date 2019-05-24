5430ec7eb6b03958cf4ec41bb7d4ff2045444822
zhengdongqi <1821260963@qq.com>
2019-05-24 10:52:11 +0800

    求面积的问题

---
+++ 108.c
@@ -0,0 +1,24 @@
+/*************************************************************************
+	> File Name: 108.c
+	> Author: zhengdongqi
+	> Mail: 1821260963@qq.com
+	> Created Time: 四  5/23 23:29:21 2019
+ ************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    char c;
+    double a, b;
+    scanf("%c", &c);
+    scanf("%lf %lf", &a, &b);
+    if (c == 'r') {
+        printf("%.2lf\n", a * b);
+    } else if (c == 't') {
+        printf("%.2lf\n", a * b / 2);
+    }
+    return 0;
+}
