5d6bc8ab1ad66257214526f775db7af77716daed
zhengdongqi <1821260963@qq.com>
2019-05-24 10:52:47 +0800

    四位数中有偶数位么

---
+++ 109.c
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: 109.c
+	> Author: zhengdongqi
+	> Mail: 1821260963@qq.com
+	> Created Time: 四  5/23 23:29:39 2019
+ ************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    while (n) {
+        if (((n % 10) % 2) == 0) {
+            printf("YES\n");
+            return 0;
+        }
+        n /= 10;
+    }
+    printf("NO\n");
+    return 0;
+}
+
