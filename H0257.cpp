dfba930140b79c0f8141f4a41851844530141767
zhengdongqi <1821260963@qq.com>
2019-06-20 20:36:42 +0800

    树的颜色

--- H0257.cpp
+++ H0257.cpp
@@ -6,6 +6,7 @@
  ************************************************************************/
 
 #include <iostream>
+#include <algorithm>
 #include <queue>
 using namespace std;
 #define MAX_N 1000
@@ -15,40 +16,41 @@ struct Node {
     int val, cnt;
 };
 
-priority_queue<Node> q;
 bool operator<(const Node &a, const Node &b) {
-    return a.val * b.cnt < b.val * a.cnt;
+    return a.val * b.cnt < a.cnt * b.val;
 }
 
-struct Unionset {
+struct UnionSet {
     int fa[MAX_N + 5];
     Node arr[MAX_N + 5];
-    int next[MAX_N + 5];
+    int ans[MAX_N + 5];
     inline void init(int n) {
-        for (int i = 1; i <= n; i++) {
-            fa[i] = i, next[i] = 0;
+        for (int i = 1 ; i <= n; i++) {
+            fa[i] = i;
+            ans[i] = 0;
+            arr[i].father = 0;
             arr[i].head = arr[i].tail = i;
             arr[i].cnt = 1;
-            arr[i].father = 0;
         }
-        return ;
     }
     int get(int x) {
-        return fa[x] = (fa[x] == x ? x : get(fa[x]));
+        return (fa[x] == x ? x : get(fa[x]));
     }
     void merge(int a, int b) {
         int aa = get(a), bb = get(b);
         if (aa == bb) return ;
         fa[bb] = aa;
-        next[arr[aa].tail] = arr[bb].head;
+        ans[arr[aa].tail] = arr[bb].head;
         arr[aa].tail = arr[bb].tail;
-        arr[aa].val = arr[bb].val; 
-        arr[aa].cnt = arr[bb].cnt;
+        arr[aa].val += arr[bb].val;
+        arr[aa].cnt += arr[bb].cnt;
         return ;
     }
 };
 
-Unionset u;
+UnionSet u;
+priority_queue<Node> que;
+
 int val[MAX_N + 5];
 
 int main() {
@@ -56,29 +58,27 @@ int main() {
     cin >> n >> r;
     u.init(n);
     for (int i = 1; i <= n; i++) {
-        cin >> u.arr[i].val;
-        val[i] = u.arr[i].val;
+        cin >> val[i];
+        u.arr[i].val = val[i];
+        que.push(u.arr[i]);
     }
-    for (int i = 1; i< n; i++) {
+    for (int i = 1; i < n; i++) {
         int a, b;
         cin >> a >> b;
         u.arr[b].father = a;
     }
-    for (int i = 1; i <= n; i++) {
-        q.push(u.arr[i]);
-    }
-    while (!q.empty()) {
-        int fp = q.top().father, p = q.top().head;
-        q.pop();
-        if (fp) {
-            u.merge(fp, p);
-            q.push(u.arr[u.get(p)]);
-        }
+    while (!que.empty()) {
+        int ind = que.top().head, fa = u.arr[ind].father;
+        que.pop();
+        if (fa == 0) continue;
+        u.merge(fa, ind);
+        que.push(u.arr[u.get(ind)]);
     }
     int p = r, t = 1, ans = 0;
     while (p) {
-        ans += val[p] * (t++);
-        p = u.next[p];
+        ans += val[p] * t;
+        p = u.ans[p];
+        t++;        
     }
     cout << ans << endl;
     return 0;
