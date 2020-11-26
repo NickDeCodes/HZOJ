8822765ba2b068c3679e4839ccd47e60e9dfda20
zhengdongqi <zhengdongqi@huohua.cn>
2020-11-26 10:35:22 +0800

    H0397.cpp

---
+++ H0397.cpp
@@ -0,0 +1,43 @@
+/*************************************************************************
+	> File Name: H0397.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 日 10/11 11:29:05 2020
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int n, m, mmap[105][105], ans = 0;
+int dir[4][2] = {0, 1, 1, 0, 0, -1, -1, 0};
+
+void func(int x, int y) {
+    for (int i = 0; i < 4; i++) {
+        int tx = x + dir[i][0];
+        int ty = y + dir[i][1];
+        if (mmap[tx][ty]) {
+            mmap[tx][ty] = 0;
+            func(tx, ty);
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> mmap[i][j];
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (mmap[i][j]) {
+                ans++;
+                mmap[i][j] = 0;
+                func(i, j);
+            }
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
